9ade40db62aecd88385b38a8fc05536cdf9d6b98
Thomas Fritzmann <thfritzmann@gmail.com>
2022-12-15 22:09:54 -0600

    Changed all the links to youtube.

--- __scripts__/engine.js
+++ __scripts__/engine.js
@@ -1,69 +1,48 @@
-if (typeof(Storage)!== undefined){
-    
-    var currentStyleSheet = document.getElementById("currentTheme");
-    var backgroundVideo = document.getElementById("currentBackground");
+if (typeof Storage !== undefined) {
+  var currentStyleSheet = document.getElementById("currentTheme");
+  var backgroundVideo = document.getElementById("currentBackground");
 
-    if(currentStyleSheet.href.contains == "light" || window.localStorage.getItem("theme") == "true"){
-        window.localStorage.setItem("theme", true);
-        $(".logo").css("transform","rotate(360deg)");
-        $("html").fadeOut("fast", function(){
-            currentStyleSheet.href = "__style__\\style_light.css";
-            backgroundVideo.innerHTML = "";
-            backgroundVideo.innerHTML = '<source src="__site_assets__\\grass_animation_lightTheme_1080p.mp4" type="video/mp4">';
-            backgroundVideo.load();
-            $("html").fadeIn("slow");
-        });
-    }
-    else{
-        window.localStorage.setItem("theme", false);
-        $(".logo").css("transform","rotate(180deg)");
-        $("html").fadeOut("fast", function(){
-            currentStyleSheet.href = "__style__\\style_dark.css";
-            backgroundVideo.innerHTML = "";
-            backgroundVideo.innerHTML = '<source src="__site_assets__\\grass_animation_darkTheme_1080p.mp4" type="video/mp4">';
-            backgroundVideo.load();
-            $("html").fadeIn("slow");
-        });
-    }
-}
-else{
-    alert("Your browser does not support local storage. The current theme will not be saved.")
+  if (
+    currentStyleSheet.href.contains == "light" ||
+    window.localStorage.getItem("theme") == "true"
+  ) {
+    window.localStorage.setItem("theme", true);
+    $("html").fadeOut("slow", function () {
+      currentStyleSheet.href = "__style__\\style_light.css";
+      $("html").fadeIn("slow");
+    });
+  } else {
+    window.localStorage.setItem("theme", false);
+    $(".logo").css("transform", "rotate(180deg)");
+    $("html").fadeOut("slow", function () {
+      currentStyleSheet.href = "__style__\\style_dark.css";
+      $("html").fadeIn("slow");
+    });
+  }
+} else {
+  alert(
+    "Your browser does not support local storage. The current theme will not be saved."
+  );
 }
 
-var currentThemeButton =  document.getElementById("logoJS");
+var currentThemeButton = document.getElementById("logoJS");
 
-currentThemeButton.onclick = function(){
-
-    var currentTheme = window.localStorage.getItem("theme");
-    var currentStyleSheet = document.getElementById("currentTheme");
-    var backgroundVideo = document.getElementById("currentBackground");
-    console.log(currentBackground.src);
-    if (currentTheme == "true"){
-        window.localStorage.setItem("theme", false);
-        $(".logo").css("transform","rotate(180deg)");
-        $("html").fadeOut("slow", function(){
-            currentStyleSheet.href = "__style__\\style_dark.css";
-
-            backgroundVideo.innerHTML = "";
-            backgroundVideo.innerHTML = '<source src="__site_assets__\\grass_animation_darkTheme_1080p.mp4" type="video/mp4">';
-            backgroundVideo.load();
-
-            $("html").fadeIn("slow");
-        });
-    }
-    else{
-        window.localStorage.setItem("theme", true);
-        $(".logo").css("transform","rotate(360deg)");
-        $("html").fadeOut("slow", function(){
-            currentStyleSheet.href = "__style__\\style_light.css";
-
-            backgroundVideo.innerHTML = "";
-            backgroundVideo.innerHTML = '<source src="__site_assets__\\grass_animation_lightTheme_1080p.mp4" type="video/mp4">';
-            backgroundVideo.load();
-
-            $("html").fadeIn("slow");
-        });
-    }
-}
-
-
+currentThemeButton.onclick = function () {
+  var currentTheme = window.localStorage.getItem("theme");
+  var currentStyleSheet = document.getElementById("currentTheme");
+  if (currentTheme == "true") {
+    window.localStorage.setItem("theme", false);
+    $(".logo").css("transform", "rotate(180deg)");
+    $("html").fadeOut("slow", function () {
+      currentStyleSheet.href = "__style__\\style_dark.css";
+      $("html").fadeIn("slow");
+    });
+  } else {
+    window.localStorage.setItem("theme", true);
+    $(".logo").css("transform", "rotate(360deg)");
+    $("html").fadeOut("slow", function () {
+      currentStyleSheet.href = "__style__\\style_light.css";
+      $("html").fadeIn("slow");
+    });
+  }
+};
